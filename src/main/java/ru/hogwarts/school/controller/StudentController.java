e19a0efbbdad09f9a8d77851b998d669d2faf52b
Igor Gunin <gunin-igor75@yandex.ru>
2022-12-07 12:29:22 +0600

    stream file bd

--- src/main/java/ru/hogwarts/school/controller/StudentController.java
+++ src/main/java/ru/hogwarts/school/controller/StudentController.java
@@ -1,11 +1,25 @@
 package ru.hogwarts.school.controller;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import ru.hogwarts.school.model.Avatar;
 import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.service.AvatarService;
 import ru.hogwarts.school.service.StudentService;
 import ru.hogwarts.school.model.Student;
 
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Objects;
@@ -15,9 +29,11 @@ import java.util.Objects;
 public class StudentController {
 
     private final StudentService studentService;
+    private final AvatarService avatarService;
 
-    public StudentController(StudentService studentService) {
+    public StudentController(StudentService studentService, AvatarService avatarService) {
         this.studentService = studentService;
+        this.avatarService = avatarService;
     }
 
     @GetMapping("{id}")
@@ -75,5 +91,38 @@ public class StudentController {
     public ResponseEntity<Faculty> getFacultyStudent(@PathVariable long id) {
         Faculty faculty = studentService.findFaculty(id);
         return ResponseEntity.ok(faculty);
+    }
+
+    @PostMapping(value = "{id}/avatar", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> uploadAvatar(@PathVariable long id,
+                                               @RequestParam MultipartFile file) throws IOException {
+        if (file.getSize() > 1024 * 300) {
+            return ResponseEntity.badRequest().body("File is to big");
+        }
+        avatarService.uploadAvatar(id, file);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping(value = "{id}/avatar-from-db")
+    public ResponseEntity<byte[]> downLoadAvatar(@PathVariable long id) throws Exception {
+        Avatar avatar = avatarService.findAvatarById(id);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.parseMediaType(avatar.getMediaType()));
+        headers.setContentLength(avatar.getData().length);
+        return ResponseEntity.status(HttpStatus.OK).headers(headers).body(avatar.getData());
+    }
+
+    @GetMapping(value = "{id}/avatar-from-file")
+    public void downLoadAvatar(@PathVariable long id, HttpServletResponse response) throws Exception {
+        Avatar avatar = avatarService.findAvatarById(id);
+        Path path = Paths.get(avatar.getFilePath());
+        try (InputStream in = Files.newInputStream(path);
+             OutputStream out = response.getOutputStream();
+        ) {
+            response.setStatus(200);
+            response.setContentType(avatar.getMediaType());
+            response.setContentLength((int) avatar.getFileSize());
+            in.transferTo(out);
+        }
     }
 }
---
+++ src/main/java/ru/hogwarts/school/model/Avatar.java
@@ -0,0 +1,69 @@
+package ru.hogwarts.school.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Avatar {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String filePath;
+    private String mediaType;
+
+    private long fileSize;
+    @Lob
+    private byte[] data;
+    @OneToOne(cascade = CascadeType.ALL)
+    private Student student;
+
+    public Avatar() {
+    }
+
+    public long getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(long fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public String getMediaType() {
+        return mediaType;
+    }
+
+    public void setMediaType(String mediaType) {
+        this.mediaType = mediaType;
+    }
+
+    public byte[] getData() {
+        return data;
+    }
+
+    public void setData(byte[] data) {
+        this.data = data;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+}
